44d745579825d8f95da903b1e76841a65b492940
Arshiya Sethi <31984182+arshiya2704@users.noreply.github.com>
2017-10-20 18:09:19 -0700

    change12

--- dropboxreact_ui/src/Components/Register.js
+++ dropboxreact_ui/src/Components/Register.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
 import img from '../img.png';
 import * as API from '../api/API';
+import logo from '../logo.png';
 
 class Register extends Component {
     constructor() {
@@ -35,26 +36,28 @@ class Register extends Component {
 
     render() {
         return (
-            <div className="container-fluid">
-                <div className="card card-size mx-auto p-5">
-                    <div className="row">
-                        <div className="col-sm- col-md-6">
-                            <img src={img} alt="logo"/>
-                        </div>
-                        <div className="col-sm-4 col-md-4">
-                            Create an account
-                            <hr/>
-                            <form action="">
-                                <input type="text" className="form-control" placeholder="First name" onChange={this.handleChange.bind(this, 'Fname')} value={this.state.formData.Fname}/><br/>
-                                <input type="text" className="form-control" placeholder="Surname" onChange={this.handleChange.bind(this, 'Lname')} value={this.state.formData.Lname}/><br/>
-                                <input type="email" className="form-control" placeholder="Email" onChange={this.handleChange.bind(this, 'email')} value={this.state.formData.email}/><br/>
-                                <input type="password" className="form-control" placeholder="Password" onChange={this.handleChange.bind(this, 'password')} value={this.state.formData.password}/><br/><br/>
-                                <button type="submit" className="btn btn-primary" onClick={(e) => (e.preventDefault(), this.handleSubmit(this.state))}>Create an account</button>
-                            </form>
-                        </div>
+                <div className="row justify-content-md-center">
+                    <div className="col">
+                        <h1 className="text-center">
+                            <img src={logo} alt="logo"/>Dropbox
+                        </h1>
+                        <hr/>
+                    </div>
+                    <div className="col-md-3 col-md-offset-3 col-sm-5 col-sm-offset-1 col-xs-6">
+                        <img src={img} className="img-responsive" alt="logo"/>
+                    </div>
+                    <div className="col-md-4 col-md-offset-0 col-sm-5 col-sm-offset-0 col-xs-6">
+                        Create an account
+                        <hr/>
+                        <form action="">
+                            <input type="text" className="form-control" placeholder="First name" onChange={this.handleChange.bind(this, 'Fname')} value={this.state.formData.Fname}/><br/>
+                            <input type="text" className="form-control" placeholder="Surname" onChange={this.handleChange.bind(this, 'Lname')} value={this.state.formData.Lname}/><br/>
+                            <input type="email" className="form-control" placeholder="Email" onChange={this.handleChange.bind(this, 'email')} value={this.state.formData.email}/><br/>
+                            <input type="password" className="form-control" placeholder="Password" onChange={this.handleChange.bind(this, 'password')} value={this.state.formData.password}/><br/><br/>
+                            <button type="submit" className="btn btn-primary" onClick={(e) => (e.preventDefault(), this.handleSubmit(this.state))}>Create an account</button>
+                        </form>
                     </div>
                 </div>
-            </div>
         );
     }
 }
