2ea7be2834293208f892767a032c526f2bece796
Arshiya Sethi <31984182+arshiya2704@users.noreply.github.com>
2017-10-20 18:08:09 -0700

    change9

--- dropboxreact_ui/src/Components/Error1.js
+++ dropboxreact_ui/src/Components/Error1.js
@@ -2,23 +2,28 @@ import React, {Component} from 'react';
 import {withRouter,Link} from 'react-router-dom';
 import img from '../img.png';
 import imgerr from '../error.png';
+import logo from '../logo.png';
 
 class Error extends Component {
     render() {
         return (
-            <div className="container-fluid">
-                <div className="card card-size mx-auto p-5">
+            <div className="row justify-content-md-center">
+                <div className="col">
+                    <h1 className="text-center">
+                        <img src={logo} alt="logo"/>Dropbox
+                    </h1>
+                    <hr/>
+                </div>
                     <div className="row">
-                        <div className="col-sm- col-md-6">
-                            <img src={img} alt="logo"/>
+                        <div className="col-md-3 col-md-offset-3 col-sm-5 col-sm-offset-1 col-xs-6">
+                            <img src={img} className="img-responsive" alt="logo"/>
                         </div>
-                        <div className="col-sm-4 col-md-4">
+                        <div className="col-md-4 col-md-offset-0 col-sm-5 col-sm-offset-0 col-xs-6">
                             <img src={imgerr} alt="error" height={20}/>
                             <label className="text-danger">First Name,Email and Password are mandatory fields</label><br/><br/>
                             <Link to="/register">Go back</Link>
                         </div>
                     </div>
-                </div>
             </div>
         );
     }
