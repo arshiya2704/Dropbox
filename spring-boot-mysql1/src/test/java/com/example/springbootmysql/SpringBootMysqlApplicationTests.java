925fbb4ef74e0dde7ee0067150802627eb45e270
Arshiya Sethi <31984182+arshiya2704@users.noreply.github.com>
2017-12-11 14:36:44 -0800

    junit

--- spring-boot-mysql1/src/test/java/com/example/springbootmysql/SpringBootMysqlApplicationTests.java
+++ spring-boot-mysql1/src/test/java/com/example/springbootmysql/SpringBootMysqlApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.springbootmysql;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,55 @@ public class SpringBootMysqlApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+	}
+	@Before
+	public void setup()
+	{
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+				.addFilters(springSecurityFilterChain)
+				.build();
+	}
+	@Test
+	public void verifiesLoginPageLoads() throws Exception
+	{
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(MockMvcResultMatchers.model().hasNoErrors())
+				.andExpect(MockMvcResultMatchers.view().name("login"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	public void testUserLogin()  throws Exception
+	{
+		HttpSession session = mockMvc.perform(post("/login")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("username", "test")
+				.param("password", "test123")
+		)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				//.andExpect(redirectedUrl("/user/home"))
+				.andReturn()
+				.getRequest()
+				.getSession();
+
+		request.setSession(session);
+
+		SecurityContext securityContext = (SecurityContext)   session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+
+		SecurityContextHolder.setContext(securityContext);
+	}
+
+	@Test
+	public void testRetrieveUserBookings() throws Exception
+	{
+		testUserLogin();
+
+		mockMvc.perform(MockMvcRequestBuilders.get("user/bookings"))
+				.andExpect(MockMvcResultMatchers.model().hasNoErrors())
+				.andExpect(MockMvcResultMatchers.model().attributeExists("bookings"))
+				.andExpect(MockMvcResultMatchers.view().name("user/bookings"))
+				.andExpect(content().string(containsString("Booking")));
 	}
 
 }
